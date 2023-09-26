837212d0bebc332d7948bafc19e14c498261a6c1
Igor Oberman <oberman.ie@gmail.com>
2023-09-26 03:07:18 +0300

    Initial commit

---
+++ ir/const.h
@@ -0,0 +1,79 @@
+#pragma once
+
+namespace compiler
+{
+
+enum class InstType
+{
+    NONE = 0,
+    BINARY,
+    UNARY,
+    CONST,
+    PARAM,
+    JUMP,
+    CALL,
+    MOV,
+    CAST,
+    CMP,
+    PHI,
+    END = PHI
+};
+
+enum class BinOpType
+{
+    NONE = 0,
+
+	// Arithmetical
+    ADD, 
+    SUB, 
+    MUL, 
+    DIV, 
+    MOD,
+
+    // Bitwise
+    SHL,
+    SHR,
+
+    // Logical
+	AND, 
+	OR, 
+	XOR,
+
+    // Compare
+    CMP,
+    END = CMP
+};
+
+enum class UnOpType
+{
+    NONE = 0,
+    NOT,
+    MOV,
+    RETURN,
+    END = RETURN
+};
+
+enum class JumpOpType
+{
+    NONE = 0,
+    JMP,
+    JE,
+    JNE,
+    JLT,
+    JLE,
+    JGT,
+    JGE,
+    END = JGE
+};
+
+enum class DataType
+{
+    NONE = 0,
+    INT32,
+    INT64,
+    FLOAT32,
+    FLOAT64,
+    END = FLOAT64
+};
+
+}
